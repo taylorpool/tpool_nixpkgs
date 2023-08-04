8e2193eb575b0867449a294df8c6cbd2da88d00e
Taylor Pool <tpool@andrew.cmu.edu>
2023-08-04 14:14:15 -0400

    Fixed typo

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@ self: super:
 {
   gtsam = super.callPackage ./pkgs/gtsam {};
   lz4 = super.callPackage ./pkgs/lz4 {};
-  mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4; }
+  mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4; };
 }
