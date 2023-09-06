ec27e6efd4bafdcc42efab3ac7bf720371d61cba
Taylor Pool <tpool@andrew.cmu.edu>
2023-09-06 10:05:41 -0400

    Added velodyne_decoder

--- default.nix
+++ default.nix
@@ -3,4 +3,5 @@ self: super:
   gtsam = super.callPackage ./pkgs/gtsam {};
   lz4cmake = super.callPackage ./pkgs/lz4 {};
   mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4cmake; };
+  velodyne_decoder = import ./pkgs/velodyne_decoder { pkgs=super; };
 }
