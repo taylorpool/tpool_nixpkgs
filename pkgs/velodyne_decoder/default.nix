a22735f1cf0e0df715906f2d9e9c9ed2ade7cd3f
Taylor Pool <tpool@andrew.cmu.edu>
2023-09-23 23:54:17 -0400

    More updates to velodyne_decoder

--- pkgs/velodyne_decoder/default.nix
+++ pkgs/velodyne_decoder/default.nix
@@ -1,6 +1,6 @@
 {pkgs}:
 ( import ( builtins.fetchGit {
-  url = "git@bitbucket.org:tpool23/velodyne_decoder.git";
+  url = "git@bitbucket.org-cmu:tpool23/velodyne_decoder.git";
   rev = "c37ae9cc860bcfcdc61f8dccd935f27a7c7c7374";
   ref = "master";
 } ) { pkgs=pkgs; } )
--- version.nix
+++ version.nix
@@ -1,1 +1,6 @@
-import ( builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz" ) {}
+import ( builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz" ) {
+  overlays = [
+    ( import ( ( builtins.fetchTarball "https://github.com/lopsided98/nix-ros-overlay/archive/master.tar.gz"
+    ) + "/overlay.nix" ) )
+  ];
+}
