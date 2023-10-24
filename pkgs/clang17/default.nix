aa1e1b3cb48904c0de28bb06f56ac6f922f76870
Taylor Pool <tpool@andrew.cmu.edu>
2023-10-24 19:34:25 -0400

    Added support for llvm17

--- pkgs/clang17/default.nix
+++ pkgs/clang17/default.nix
@@ -1,12 +1,13 @@
 { lib, stdenv, cmake, ninja }:
 stdenv.mkDerivation {
   pname = "llvm";
-  version = "17.0.1";
+  version = "17.0.3";
 
-  src = builtins.fetchTarball "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.1/llvm-project-17.0.1.src.tar.xz";
+  src = builtins.fetchTarball "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.3/llvm-project-17.0.3.src.tar.xz";
 
   cmakeFlags = [ 
-    "-DCMAKE_BUILD_TYPE=Release" "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
     "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
     "-DLIBCXX_ENABLE_STD_MODULES=ON"
     "-G Ninja"
