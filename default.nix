fdaa432d9a962c93dfe3cba1e830141ac8f15b7f
Taylor Pool <tpool@andrew.cmu.edu>
2023-11-08 12:48:59 -0500

    Added llvm17

--- default.nix
+++ default.nix
@@ -1,8 +1,21 @@
 self: super:
 {
   gtsam = super.callPackage ./pkgs/gtsam {};
+
   lz4cmake = super.callPackage ./pkgs/lz4 {};
+
   mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4cmake; };
+
   velodyne_decoder = import ./pkgs/velodyne_decoder { pkgs=super; };
-  clang17 = super.callPackage ./pkgs/clang17 {};
+
+  llvmPackages_17 = recurseIntoAttrs (callPackage ./pkgs/llvm17 ({
+    inherit (stdenvAdapters) overrideCC;
+    buildLlvmTools = buildPackages.llvmPackages_17.tools;
+    targetLlvmLibraries = targetPackages.llvmPackages_17.libraries or llvmPackages_17.libraries;
+    targetLlvm = targetPackages.llvmPackages_17.llvm or llvmPackages_17.llvm;
+  }));
+
+  clang-tools_17 = super.callPackage ./pkgs/clang-tools {
+    llvmPackages = self.llvmPackages_17;
+  };
 }
