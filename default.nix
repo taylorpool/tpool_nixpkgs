c1a981cc855c30057caddd3a76d8dd712230a98a
Taylor Pool <tpool@andrew.cmu.edu>
2023-11-13 15:26:22 -0500

    removed llvm17

--- default.nix
+++ default.nix
@@ -9,15 +9,4 @@ self: super:
   mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4cmake; };
 
   velodyne_decoder = import ./pkgs/velodyne_decoder { pkgs=super; };
-
-  llvmPackages_17 = recurseIntoAttrs (callPackage ./pkgs/llvm17 ({
-    inherit (stdenvAdapters) overrideCC;
-    buildLlvmTools = buildPackages.llvmPackages_17.tools;
-    targetLlvmLibraries = targetPackages.llvmPackages_17.libraries or llvmPackages_17.libraries;
-    targetLlvm = targetPackages.llvmPackages_17.llvm or llvmPackages_17.llvm;
-  }));
-
-  clang-tools_17 = super.callPackage ./pkgs/clang-tools {
-    llvmPackages = self.llvmPackages_17;
-  };
 }
