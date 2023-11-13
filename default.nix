a02187372b32abe7937c96e4f0e1575e47de74c5
Taylor Pool <tpool@andrew.cmu.edu>
2023-11-13 15:25:02 -0500

    robin map

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@ self: super:
 {
   gtsam = super.callPackage ./pkgs/gtsam {};
 
+  robin_map = super.callPackage ./pkgs/robin_map {};
+
   lz4cmake = super.callPackage ./pkgs/lz4 {};
 
   mcap = super.callPackage ./pkgs/mcap { lz4=self.lz4cmake; };
---
+++ pkgs/robin_map/default.nix
@@ -0,0 +1,15 @@
+{ lib, stdenv, cmake, ninja }:
+stdenv.mkDerivation {
+	pname = "robin_map";
+	version = "0.1.0";
+
+	src = builtins.fetchGit {
+		url = "git@github.com:Tessil/robin-map.git";
+		ref = "master";
+		rev = "5eace6f74c9edff8e264c2d26a85365ad9ea149c";
+	};
+
+	meta = with lib; {
+		description = ""
+	};
+}
