d8f0eff7b99986fc9a864012fb6fd37c5ded42e8
Taylor <tpool@andrew.cmu.edu>
2023-11-21 13:16:05 -0500

    Update default.nix

--- pkgs/velodyne_decoder/default.nix
+++ pkgs/velodyne_decoder/default.nix
@@ -1,6 +1,6 @@
 {pkgs}:
 ( import ( builtins.fetchGit {
-  url = "git@bitbucket.org-cmu:tpool23/velodyne_decoder.git";
+  url = "git@github.com:taylorpool/velodyne_decoder.git";
   rev = "211009d4fe863a9e35373807f1d523a1f0aac199";
   ref = "master";
 } ) { pkgs=pkgs; } )
